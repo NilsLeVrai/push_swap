8ead7ae5cb689e35adc85cd7ccd02232cfbdddc4
NilsLeVrai <nilslucas@wanadoo.fr>
2024-01-30 16:40:27 +0100

    start

--- main_push_swap.c
+++ main_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:34:33 by niabraha          #+#    #+#             */
-/*   Updated: 2024/01/30 14:55:46 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/01/30 15:23:17 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,11 @@
 
 int main (int argc, char **argv)
 {
-    //struct    *stack_a;
-    //struct    *stack_b;
-    int         i;
+    t_list  *stack_a;
+    t_list  *stack_b;
+    int     i;
 
-    //stack_a = NULL;
-    //stack_b = NULL;
+    stack_a = NULL;
+    stack_b = NULL;
     i = 0;
-    if (argc == 1)
-        return (0);
-    while (argv[i])
-    {
-        i++;
-    }
-    
-    return (0);
 }
