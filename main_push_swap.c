de35dd63d9f6deaaa4b93926667df89705147b08
NilsLeVrai <nilslucas@wanadoo.fr>
2024-02-01 16:54:24 +0100

    start

--- main_push_swap.c
+++ main_push_swap.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:34:33 by niabraha          #+#    #+#             */
-/*   Updated: 2024/02/01 15:22:24 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/02/01 16:44:16 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ int	main (int argc, char **argv)
 		write(1, "Error\n", 6);
 	while (nb_args < argc)
 	{
-		
+		printf("oui %s", argv[nb_args]);
+		args.value[args.count++] = ft_atoi(argv[nb_args]);
 		nb_args++;
+		printf("qw %d\n", *args.value);
 	}
+	printf("yo ma gueule%d\n", args.value[4]);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 15:35:24 by niabraha          #+#    #+#             */
-/*   Updated: 2024/02/01 15:57:58 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/02/01 16:48:33 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+
+int	ft_atoi(const char *nptr);
+void    parsing_error(void);
+
+struct arguments {
+	int	count; // number of arguments
+	int	*value; // value of each arguments
+} struct_args;
 
 #endif
