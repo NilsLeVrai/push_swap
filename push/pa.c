82e63ed04f03c70a5bd05ee7d8bea997c2285455
NilsLeVrai <nilslucas@wanadoo.fr>
2024-02-21 18:47:22 +0100

    tests looking good

--- push/pa.c
+++ push/pa.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:45:49 by niabraha          #+#    #+#             */
-/*   Updated: 2024/02/21 16:46:21 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/02/21 18:19:00 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 void	pa(t_mono **a_stack, t_mono **b_stack)
 {
-	t_mono	*tmp;
+	t_mono	*temp;
 
-	if (*b_stack)
-	{
-		tmp = *b_stack;
-		*b_stack = (*b_stack)->next;
-		tmp->next = *a_stack;
-		*a_stack = tmp;
-	}
+	if (*b_stack == NULL)
+		return ;
+	temp = *b_stack;
+	*b_stack = (*b_stack)->next;
+	temp->next = *a_stack;
+	*a_stack = temp;
 }
--- push/push_swap.c
+++ push/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:33:38 by niabraha          #+#    #+#             */
-/*   Updated: 2024/02/21 17:11:08 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/02/21 18:45:13 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,83 @@ int	main(int argc, char **argv)
 {
 	struct poly	*a_stack;
 	struct poly	*b_stack;
+	int i = 1;
 
 	error_check(argc, argv);
 	a_stack = create_linked_list(argc, argv);
 	b_stack = NULL;
-	b_stack++;
-	printf("Before:\n");
-	printf("A:\n");
+	printf("Beginning:\n");
+	printf("A: ");
 	print_linked_list(a_stack);
+	printf("B: ");
+	printf("\n");
+	print_linked_list(b_stack);
+
 	pb(&a_stack, &b_stack);
-	printf("A:\n");
+	printf("pb\n");
+	printf("[%d] A: ", i);
 	print_linked_list(a_stack);
-	printf("B:\n");
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+
+	pb(&a_stack, &b_stack);
+	printf("pb\n");
+	printf("[%d] A: ", i);
+	print_linked_list(a_stack);
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+
+	ra(&a_stack);
+	printf("ra\n");
+	printf("[%d] A: ", i);
+	print_linked_list(a_stack);
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+
+	rb(&b_stack);
+	printf("rb\n");
+	printf("[%d] A: ", i);
+	print_linked_list(a_stack);
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+
+	rr(&a_stack, &b_stack);
+	printf("rr\n");
+	printf("[%d] A: ", i);
+	print_linked_list(a_stack);
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+
+	sa(&a_stack);
+	printf("sa\n");
+	printf("[%d] A: ", i);
+	print_linked_list(a_stack);
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+
+	sb(&b_stack);
+	printf("sb\n");
+	printf("[%d] A: ", i);
+	print_linked_list(a_stack);
+	printf("[%d] B: ",i);
+	print_linked_list(b_stack);
+	printf("\n");
+	i++;
+	
+	printf("End A:\n");
+	print_linked_list(a_stack);
+	printf("End B:\n");
 	print_linked_list(b_stack);
 }
