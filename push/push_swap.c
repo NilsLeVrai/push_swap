354b3518603ad8e1528acd2c2aa54adc89860374
Nils Abraham <nils@sargasso.home>
2024-04-02 00:39:02 +0200

    visu

--- push/push_swap.c
+++ push/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 14:33:38 by niabraha          #+#    #+#             */
-/*   Updated: 2024/03/30 16:01:57 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/04/02 00:01:40 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,16 @@ int	main(int argc, char **argv)
 	error_check(argc, argv);
 	a_stack = create_list(argc, argv);
 	b_stack = NULL;
-	printf("a_stack:\n");
+	pb(&a_stack, &b_stack);
+	sa(&a_stack);
+	rra(&a_stack);
+	sa(&a_stack);
+	pa(&a_stack, &b_stack);
+	rra(&a_stack);
+
+/* 	printf("a_stack:\n");
 	print_index(a_stack);
 	printf("b_stack:\n");
-	print_index(b_stack);
+	print_index(b_stack); */
 	//free_lst(a_stack, b_stack);
 }
