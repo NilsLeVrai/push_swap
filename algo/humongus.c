4d6ec6f80e51d6e91dd9dfce4694cd2f07638964
NilsLeVrai <nilslucas@wanadoo.fr>
2024-04-16 21:04:58 +0200

    5 groups TODO

--- algo/humongus.c
+++ algo/humongus.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 17:52:57 by niabraha          #+#    #+#             */
-/*   Updated: 2024/04/16 19:46:40 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/04/16 20:51:13 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,32 +19,55 @@ void humongus_sort(t_index **a_stack, t_index **b_stack)
 
 	temp_a = *a_stack;
 	temp_b = *b_stack;
-	int len = lst_size(temp_a);
+	int len_a = lst_size(temp_a);
+	int len_b;
 	printf("len = %d\n", len);
 	if (temp_b)
 	{
 		write(1, "blabla\n", 7);
 	}
-	while (len > 0)
+	int cost = 0;
+	while (len_a > 0)
 	{
 		if (temp_a->group == 1)
 		{
 			pb(a_stack, b_stack);
 			rb(b_stack);
 		}
+		else if (temp_a->group == 2)
+		{
+			pb(a_stack, b_stack);
+			len_b = lst_size(temp_b);
+			while (temp_b && temp_b->group != 1)
+			{
+				cost++;
+				temp_b = temp_b->next;
+			}
+			temp_b = *b_stack;
+			if (cost > len_b / 2)
+			{
+				while (temp_b->group != 1)
+				{
+					rrb(b_stack);
+				}
+			}
+			else
+			{
+				while (temp_b->group != 1)
+				{
+					rb(b_stack);
+				}
+			}
+		}
 		else if (temp_a->group == 4)
 		{
 			pb(a_stack, b_stack);
-		}
-		else if (temp_a->group == 3 || temp_a->group == 2)
-		{
-			
 		}
 		else
 		{
 			ra(a_stack);
 		}
 		temp_a = *a_stack;
-		len--;
+		len_a--;
 	}
 }
