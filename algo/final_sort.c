475c83259ea2bac3e1201ff26308784f5a9f6b7f
NilsLeVrai <nilslucas@wanadoo.fr>
2024-04-26 17:31:30 +0200

    norminette almost friendly + final sort to do

--- algo/final_sort.c
+++ algo/final_sort.c
@@ -6,7 +6,7 @@
 /*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 19:24:14 by niabraha          #+#    #+#             */
-/*   Updated: 2024/04/24 19:28:32 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/04/26 17:29:32 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@ void	final_sort(t_index **a_stack, t_index **b_stack)
 {
 	(void)*a_stack;
 	(void)*b_stack;
-	return ;
+	
+	printf("(*a_stack)->index = %d\n", (*a_stack)->index);
+	printf("(*a_stack)->next->index = %d\n", (*a_stack)->next->index);
+	printf("(*a_stack)->next->next->index = %d\n", (*a_stack)->next->next->index);
+	printf("(*b_stack)->index = %d\n", (*b_stack)->index);
+	printf("(*b_stack)->next->index = %d\n", (*b_stack)->next->index);
+	ra(a_stack);
+	printf("(*a_stack)->index = %d\n", (*a_stack)->index);
+	printf("(*a_stack)->next->index = %d\n", (*a_stack)->next->index);
+	printf("(*a_stack)->next->next->index = %d\n", (*a_stack)->next->next->index);
+	printf("(*b_stack)->index = %d\n", (*b_stack)->index);
+	printf("(*b_stack)->next->index = %d\n", (*b_stack)->next->index);
+	pa(a_stack, b_stack);
+	printf("(*a_stack)->index = %d\n", (*a_stack)->index);
+	printf("(*a_stack)->next->index = %d\n", (*a_stack)->next->index);
+	printf("(*a_stack)->next->next->index = %d\n", (*a_stack)->next->next->index);
+	printf("(*b_stack)->index = %d\n", (*b_stack)->index);
+	printf("(*b_stack)->next->index = %d\n", (*b_stack)->next->index);
 }
+
--- instructions/rb.c
+++ instructions/rb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rb.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 16:49:18 by niabraha          #+#    #+#             */
-/*   Updated: 2024/03/28 16:53:50 by niabraha         ###   ########.fr       */
+/*   Updated: 2024/04/26 17:30:05 by niabraha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 void	rb(t_index **b_stack)
 {
 	t_index	*tmp;
+	t_index	*last;
 
-	tmp = *b_stack;
 	if (*b_stack && (*b_stack)->next)
 	{
+		tmp = *b_stack;
 		*b_stack = (*b_stack)->next;
-		tmp->next = (*b_stack)->next;
-		(*b_stack)->next = tmp;
+		last = *b_stack;
+		while (last->next)
+			last = last->next;
+		last->next = tmp;
+		tmp->next = NULL;
 		write(1, "rb\n", 3);
 	}
 }
