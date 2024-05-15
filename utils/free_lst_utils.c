e5cda0ad3d254ff78c91dc6dbd5dce2eb1960aed
Nils Abraham <44902217+NilsLeVrai@users.noreply.github.com>
2024-05-15 19:00:13 +0200

    Update free_lst_utils.c

--- utils/free_lst_utils.c
+++ utils/free_lst_utils.c
@@ -1,28 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   free_lst_utils.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: niabraha <niabraha@student.42mulhouse.f    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/30 12:37:56 by niabraha          #+#    #+#             */
-/*   Updated: 2024/05/01 01:55:40 by niabraha         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../includes/push_swap.h"
-
 void	free_lst(t_index **a_stack, t_index **b_stack)
 {
 	t_index	*tmp;
 
-	while (*a_stack)
+	while (a_stack && *a_stack)
 	{
 		tmp = *a_stack;
 		*a_stack = (*a_stack)->next;
 		free(tmp);
 	}
-	while (*b_stack)
+	while (b_stack && *b_stack)
 	{
 		tmp = *b_stack;
 		*b_stack = (*b_stack)->next;
